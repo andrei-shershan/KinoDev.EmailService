dbd3465828b97b02b5960a03b7804b4ff56ed345
Andrei Shershan <139045906+andrei-shershan@users.noreply.github.com>
2025-06-01 18:03:58 +0200

    Expose emails (#8)

---
+++ src/KinoDev.EmailService.WebApi/Controllers/EmailsController.cs
@@ -0,0 +1,35 @@
+using KinoDev.EmailService.WebApi.Models.RequestModels;
+using KinoDev.EmailService.WebApi.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KinoDev.EmailService.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmailsController : ControllerBase
+    {
+        private readonly IEmailSenderService _emailSenderService;
+
+        public EmailsController(IEmailSenderService emailSenderService)
+        {
+            _emailSenderService = emailSenderService;
+        }
+
+        [HttpPost("send")]
+        public async Task<IActionResult> SendEmail([FromBody] EmailRequest emailRequest)
+        {
+            if (emailRequest == null || string.IsNullOrWhiteSpace(emailRequest.To) || string.IsNullOrWhiteSpace(emailRequest.Subject) || string.IsNullOrWhiteSpace(emailRequest.Body))
+            {
+                return BadRequest("Invalid email request.");
+            }
+
+            var response = await _emailSenderService.SendAsync(emailRequest.To, emailRequest.Subject, emailRequest.Body);
+            if (response)
+            {
+                return Ok("Email sent successfully.");
+            }
+
+            return BadRequest("Failed to send email.");
+        }
+    }
+}
